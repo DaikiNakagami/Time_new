ba5533bad7543cc04d8d8f8d55e542df94521a0e
yuka asanuma <hwwmake0609@yahoo.co.jp>
2016-11-19 13:06:00 +0900

    first commit
    ベース

---
+++ TIME/MainViewController.swift
@@ -0,0 +1,55 @@
+//
+//  MainViewController.swift
+//  TIME
+//
+//  Created by nowall on 2016/11/19.
+//  Copyright © 2016年 阿部 拓磨. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    @IBOutlet weak var ageLabel: UITextField!
+    @IBOutlet weak var moneyLabel: UITextField!
+    @IBOutlet weak var hourLabel: UITextField!
+    
+    
+    @IBAction func showBtn(_ sender: UIButton) {
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
